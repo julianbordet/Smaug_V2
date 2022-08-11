54f2e213f73180d11457325722e9c1ad46658637
jbdev <bordet.julian@gmail.com>
2022-08-11 14:28:01 -0300

    Vertical Navbar buttons now correctly route to the relevant pages (using NavLink)

--- src/components/UI/VerticalNavbar.js
+++ src/components/UI/VerticalNavbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import NavbarItem from "./NavbarItem";
 import flex from '../../styles/Flex.module.css';
 import '../../styles/VerticalNavbar.css';
+import { NavLink } from 'react-router-dom';
+import { pages } from '../../util/Constants';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTachometerAlt, faBug, faTasks, faUserTag  } from '@fortawesome/free-solid-svg-icons'
@@ -14,25 +16,33 @@ const VerticalNavbar = (props) =>{
     return(
         <div className={`${flex.column} ${classes}`}>
 
-            <NavbarItem className={` ${flex.column} ${flex.alignCenter} navButton`}>
-                <span className='navItemTitle'>Dashboard</span>
-                <FontAwesomeIcon icon={faTachometerAlt} size="4x" />
-            </NavbarItem>
+            <NavLink className='link' to={pages.dashboardPage}>
+                <NavbarItem className={` ${flex.column} ${flex.alignCenter} navButton`}>
+                    <span className='navItemTitle'>Dashboard</span>
+                    <FontAwesomeIcon icon={faTachometerAlt} size="4x" />
+                </NavbarItem>
+            </NavLink>
 
-            <NavbarItem className={` ${flex.column} ${flex.alignCenter} navButton`}>
-                <span className='navItemTitle'>My Bugs</span>
-                <FontAwesomeIcon icon={faBug} size="4x" />
-            </NavbarItem>
+            <NavLink className='link' to={pages.myBugsPage}>
+                <NavbarItem className={` ${flex.column} ${flex.alignCenter} navButton`}>
+                    <span className='navItemTitle'>My Bugs</span>
+                    <FontAwesomeIcon icon={faBug} size="4x" />
+                </NavbarItem>
+            </NavLink>
 
-            <NavbarItem className={` ${flex.column} ${flex.alignCenter} navButton`}>
-                <span className='navItemTitle'>My Projects</span>
-                <FontAwesomeIcon icon={faTasks} size="4x" />
-            </NavbarItem>
+            <NavLink className='link' to={pages.myProjectsPage}>
+                <NavbarItem className={` ${flex.column} ${flex.alignCenter} navButton`}>
+                    <span className='navItemTitle'>My Projects</span>
+                    <FontAwesomeIcon icon={faTasks} size="4x" />
+                </NavbarItem>
+            </NavLink>
 
-            <NavbarItem className={` ${flex.column} ${flex.alignCenter} navButton`}>
-                <span className='navItemTitle'>Roles</span>
-                <FontAwesomeIcon icon={faUserTag} size="4x" />
-            </NavbarItem>
+            <NavLink className='link' to={pages.rolesPage}>
+                <NavbarItem className={` ${flex.column} ${flex.alignCenter} navButton`}>
+                    <span className='navItemTitle'>Roles</span>
+                    <FontAwesomeIcon icon={faUserTag} size="4x" />
+                </NavbarItem>
+            </NavLink>
 
         </div>
     )
