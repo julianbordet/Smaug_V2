e9d01dfd559f0441dea5c70ed228f671caf7ae97
jbdev <bordet.julian@gmail.com>
2022-08-15 22:18:30 -0300

    added some placeholder styling for the bug details section

--- src/components/BugDetailPanel.js
+++ src/components/BugDetailPanel.js
@@ -1,7 +1,8 @@
 import { useLocation } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchSpecificBugData } from '../store/BugActions'
+import '../styles/BugDetail.css'
 
 const BugDetailPanel = (props) =>{
 
@@ -12,7 +13,10 @@ const BugDetailPanel = (props) =>{
     //const chal = queryParams.has('detail')
     //const wasabi = queryParams.get('detail')
     const param = location.search;
-    const paramValue = param.substring(1)
+    const paramValue = param.substring(1);
+
+    const [editActive, setEditActive] = useState(false);
+
 
 
     const dispatch = useDispatch()
@@ -24,18 +28,87 @@ const BugDetailPanel = (props) =>{
     }
     ,[dispatch])
 
-
+    const editHandler = () =>{
+        setEditActive(!editActive);
+    }
 
     return(
         <div className={classes}>
-            <h1>Bug detail panel</h1>
-            {bugData.bug.bug && 
-            <div>
-                <h1>Title</h1>
-                <span>{bugData.bug.bug.title}</span>
-                <h1>Description</h1>
-                <span>{bugData.bug.bug.description}</span>
+
+            <div className='buttonsContainer'>
+                {!editActive && <button className='editButton detailsButton' onClick={editHandler}>EDIT</button>}
+                {editActive && <button className='submitButton detailsButton'>Submit</button>}
+                {editActive && <button className='cancelButton detailsButton' onClick={editHandler}>Cancel</button>}
             </div>
+            
+            {bugData.bug.bug && 
+            
+                <form className='bugDetailForm'>
+
+                    <div className='formRow'>
+                    
+                        <label className='form-label'>Title:</label>
+                        {editActive && <input className='form-input' type='text' value={bugData.bug.bug.title}></input>}
+                        {!editActive && <span className='formSpan'>{bugData.bug.bug.title}</span>}
+                    
+                        <label className='form-label'>Project:</label>
+                        {editActive && <input className='form-input' type='text' value={bugData.bug.bug.projectId}></input>}
+                        {!editActive && <span className='formSpan'>{bugData.bug.bug.projectId}</span>}
+                    
+                        <label className='form-label'>Severity:</label>
+                        {editActive && <input className='form-input' type='text' value={bugData.bug.bug.severity}></input>}
+                        {!editActive && <span className='formSpan'>{bugData.bug.bug.severity}</span>}
+                    
+                        <label className='form-label'>Priority:</label>
+                        {editActive && <input className='form-input' type='text' value={bugData.bug.bug.priority}></input>}
+                        {!editActive && <span className='formSpan'>{bugData.bug.bug.priority}</span>}
+                   
+                    </div>
+
+                    <div className='formRow'>
+
+                        <label className='form-label'>Date created:</label>
+                        {editActive && <input className='form-input' type='text' value={bugData.bug.bug.dateCreated}></input>}
+                        {!editActive && <span className='formSpan'>{bugData.bug.bug.dateCreated}</span>}
+                        
+                    
+                        <label className='form-label'>Due date:</label>
+                        {editActive && <input className='form-input' type='text' value={bugData.bug.bug.dueDate}></input>}
+                        {!editActive && <span className='formSpan'>{bugData.bug.bug.dueDate}</span>}
+                  
+                        <label className='form-label'>Assigned to:</label>
+                        {editActive && <input className='form-input' type='text' value={bugData.bug.bug.assignedTo}></input>}
+                        {!editActive && <span className='formSpan'>{bugData.bug.bug.assignedTo}</span>}
+                    
+                        <label className='form-label'>Status:</label>
+                        {editActive && <input className='form-input' type='text' value={bugData.bug.bug.isFixed}></input>}
+                        {!editActive && <span className='formSpan'>{bugData.bug.bug.isFixed}</span>}
+                       
+                    
+                        <label className='form-label'>Date fixed:</label>
+                        {editActive && <input className='form-input' type='text' value={bugData.bug.bug.dateFixed}></input>}
+                        {!editActive && <span className='formSpan'>{bugData.bug.bug.dateFixed}</span>}
+                
+                    </div>
+
+                    <div className='formRow'>
+                        <div className='form-column'>
+                            <label className='form-label'>Description:</label>
+                            {editActive && <input className='form-input' type='text' value={bugData.bug.bug.description}></input>}
+                            {!editActive && <span className='formSpan'>{bugData.bug.bug.description}</span>}
+                        </div>
+
+                        <div className='form-column'>
+                            <label className='form-label'>Steps to reproduce:</label>
+                            {editActive && <input className='form-input' type='text' value={bugData.bug.bug.stepsToReproduce}></input>}
+                            {!editActive && <span className='formSpan'>{bugData.bug.bug.stepsToReproduce}</span>}
+                        </div>
+                    </div>
+                 
+                </form>
+                    
+
+            
             }
         
 
